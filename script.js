a8c5585fa9465c1a0a8e9e31f45319792c5ed5a6
Mohit Dahiya <49828290+mohit-dahiya@users.noreply.github.com>
2023-09-02 11:12:03 +0530

    created accordion component

---
+++ script.js
@@ -0,0 +1,22 @@
+function showHide(str){
+    console.log(str);
+    document.getElementById(str).classList.add("open");
+
+    if(str === 'item1'){
+        document.getElementById('item1').classList.add("open");
+        document.getElementById('item2').classList.remove("open");
+        document.getElementById('item3').classList.remove("open");
+    }
+
+    if(str === 'item2'){
+        document.getElementById('item2').classList.add("open");
+        document.getElementById('item1').classList.remove("open");
+        document.getElementById('item3').classList.remove("open");
+    }
+
+    if(str === 'item3'){
+        document.getElementById('item3').classList.add("open");
+        document.getElementById('item1').classList.remove("open");
+        document.getElementById('item2').classList.remove("open");
+    }
+}
